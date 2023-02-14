5e709a2daf2b01da08a09b22bec845abca26cc49
S M Katibur Rahman <katiburrahmansany@gmail.com>
2023-02-14 22:26:16 +0600

    resume updated

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -6,81 +6,72 @@ import img2 from "../../Img/ui.gif";
 import img3 from "../../Img/skills.gif";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+import SMKatiburRahman from "./S M Katibur Rahman.pdf";
 
 const Services = () => {
-    // context
-    const theme = useContext(themeContext);
-    const darkMode = theme?.state?.darkMode;
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme?.state?.darkMode;
 
-    // transition
-    const transition = {
-        duration: 1,
-        type: "spring",
-    };
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
 
-    return (
-        <div className="services" id="services">
-            {/* left side */}
-            <div className="awesome">
-                {/* dark mode */}
-                <span style={{ color: darkMode ? "white" : "" }}>My Fantastic</span>
-                <span>services</span>
-                <span>
-                    I am a Web Developer with extensive experience in
-                    <br />
-                    Both Frontend development & Backend.
-                </span>
-                <a href={Resume} download>
-                    <button className="button s-button">Download Resume</button>
-                </a>
-                <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
-            </div>
-            {/* right */}
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Fantastic</span>
+        <span>services</span>
+        <span>
+          I am a Web Developer with extensive experience in
+          <br />
+          Both Frontend development & Backend.
+        </span>
+        <a href={SMKatiburRahman} download>
+          <button className="button s-button">Download Resume</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
 
-            <div className="cards">
-                {/* first card */}
-                <motion.div
-                    initial={{ left: "52rem" }}
-                    whileInView={{ left: "35rem" }}
-                    transition={transition}
-                >
-                    <Card
-                        emoji={Img1}
-                    />
-                </motion.div>
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "52rem" }}
+          whileInView={{ left: "35rem" }}
+          transition={transition}
+        >
+          <Card emoji={Img1} />
+        </motion.div>
 
+        {/* second card */}
+        <motion.div
+          initial={{ left: "2rem", top: "12rem" }}
+          whileInView={{ left: "7rem" }}
+          transition={transition}
+        >
+          <Card emoji={img2} />
+        </motion.div>
 
-                {/* second card */}
-                <motion.div
-                    initial={{ left: "2rem", top: "12rem" }}
-                    whileInView={{ left: "7rem" }}
-                    transition={transition}
-                >
-                    <Card
-                        emoji={img2}
-                    />
-                </motion.div>
-
-
-                {/* 3rd */}
-                <motion.div
-                    initial={{ top: "19rem", left: "38rem" }}
-                    whileInView={{ left: "32rem" }}
-                    transition={transition}
-                >
-                    <Card
-                        emoji={img3}
-                        color="rgba(252, 166, 31, 0.45)"
-                    />
-                </motion.div>
-                <div
-                    className="blur s-blur2"
-                    style={{ background: "var(--purple)" }}
-                ></div>
-            </div>
-        </div>
-    );
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "38rem" }}
+          whileInView={{ left: "32rem" }}
+          transition={transition}
+        >
+          <Card emoji={img3} color="rgba(252, 166, 31, 0.45)" />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
